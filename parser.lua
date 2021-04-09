4e6ce5b962ce1f8d7a19cd95f6fe38f767aa25ff
Dantevg <dantevg006@gmail.com>
2021-04-09 21:10:27 +0200

    Remove comments again, fix words with spaces
    Well that was stupid! Of course it was already in the syntax using
    labels as comments 🙄

--- parser.lua
+++ parser.lua
@@ -2,14 +2,11 @@ local interpreter = require "pancakestack.interpreter"
 
 local parser = {}
 
--- Ignore anything between brackets at the end of a line
-parser.comment = "(%(.+%))$"
-
 -- Maps statements (syntax) to functions, matching their arguments
 -- The keys are used as patterns for string.match,
 -- so non-pattern characters need to be escaped.
 parser.statements = {
-	["put this (%w+) pancake on top!"] = interpreter.putThisPancakeOnTop,
+	["put this (.+) pancake on top!"] = interpreter.putThisPancakeOnTop,
 	["eat the pancake on top!"] = interpreter.eatThePancakeOnTop,
 	["put the top pancakes together!"] = interpreter.putTheTopPancakesTogether,
 	["give me a pancake!"] = interpreter.giveMeAPancake,
@@ -18,9 +15,9 @@ parser.statements = {
 	["take from the top pancakes!"] = interpreter.takeFromTheTopPancakes,
 	["flip the pancakes on top!"] = interpreter.flipThePancakesOnTop,
 	["put another pancake on top!"] = interpreter.putAnotherPancakeOnTop,
-	["%[(%w+)%]"] = interpreter.label,
-	["if the pancake isn't tasty, go over to \"(%w+)\"%."] = interpreter.ifThePancakeIsntTastyGoOverTo,
-	["if the pancake is tasty, go over to \"(%w+)\"%."] = interpreter.ifThePancakeIsTastyGoOverTo,
+	["%[(.+)%]"] = interpreter.label,
+	["if the pancake isn't tasty, go over to \"(.+)\"%."] = interpreter.ifThePancakeIsntTastyGoOverTo,
+	["if the pancake is tasty, go over to \"(.+)\"%."] = interpreter.ifThePancakeIsTastyGoOverTo,
 	["put syrup on the pancakes!"] = interpreter.putSyrupOnThePancakes,
 	["put butter on the pancakes!"] = interpreter.putButterOnThePancakes,
 	["take off the syrup!"] = interpreter.takeOffTheSyrup,
@@ -40,12 +37,9 @@ end
 function parser.parse(input)
 	local program = {}
 	for line in input:gmatch("([^\n]+)") do
-		line = line:gsub(parser.comment, "") -- Remove comments
-		if #line > 0 then
-			local statement = parser.parseLine(line)
-			if not statement then error("Unknown statement: "..line) end
-			table.insert(program, statement)
-		end
+		local statement = parser.parseLine(line)
+		if not statement then error("Unknown statement: "..line) end
+		table.insert(program, statement)
 	end
 	return program
 end
